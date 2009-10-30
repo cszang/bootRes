fae24fd12f627135f03a7364fce09ea1766dc843
Christian Zang <christian@dedalus.local>
2009-10-30 10:29:31 +0100

    older concept of labstyles removed

--- R/dcplot.R
+++ R/dcplot.R
@@ -1,8 +1,6 @@
 dcplot <-
 function(x, ci = TRUE, sig = TRUE, labels = NULL, col = NULL, density = NULL, annot = TRUE) {
 	op <- par(no.readonly = TRUE)
-	if (labstyle == 1) {
-		par(xpd = TRUE, oma = c(3, 0.5, 0.5, 0.5))
 	} else {
 		par(xpd = TRUE)
 		labels <- rownames(x)
@@ -51,17 +49,6 @@ function(x, ci = TRUE, sig = TRUE, labels = NULL, col = NULL, density = NULL, an
 	points(1:n, x$coef, pch = pchs)
 	if (ci)
 		segments(1:n, x$ci.lower, 1:n, x$ci.upper)
-	if (labstyle == 1)
 		text((1:n)+0.01*n, rep(plot.range[1]*1.2, n), labels, srt = 90, pos = 2, cex = 0.7)
-	if (labstyle == 2) {
-		parse.lab <- function(str) {
-			split <- strsplit(str, "\\.")
-			first.letter <- substring(split[[1]][3], 1, 1)
-			if (split[[1]][2] == "prev")
-				first.letter <- toupper(first.letter)
-			first.letter
-		}
-		text((1:n)+0.02*n, rep(plot.range[1]*1.2, n), sapply(labels, parse.lab), pos = 2, cex = 1)
-	}
 	par(op)
 }
