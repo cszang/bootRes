119e0845e05623f1ce8298b11fc226d3449355e8
Christian Zang <christian@dedalus.local>
2009-10-30 10:33:04 +0100

    removed pch-selection

--- R/dcplot.R
+++ R/dcplot.R
@@ -18,32 +18,8 @@ function(x, ci = TRUE, sig = TRUE, labels = NULL, col = NULL, density = NULL, an
 	} else {
 		plot.range <- range(x$coef)
 	}
-	if (no.vars == 2) {
-		pch.1 <- rep(1, n/2)
-		pch.2 <- rep(2, n/2)
-		if (sig) {
-			pch.1[which(x$significant[1:(n/2)] == 1)] <- 19
-			pch.2[which(x$significant[(n/2 + 1):n] == 1)] <- 17
-		}
-		pchs <- c(pch.1, pch.2)
-	} else {
-		if (no.vars == 3) {
-			pch.1 <- rep(1, n/3)
-			pch.2 <- rep(2, n/3)
-			pch.3 <- rep(0, n/3)
-			if (sig) {
-				pch.1[which(x$significant[1:(n/3)] == 1)] <- 19
-				pch.2[which(x$significant[(n/3 + 1):(2*(n/3))] == 1)] <- 17
-				pch.3[which(x$significant[(2*(n/3) + 1):n] == 1)] <- 15
-			}
-			pchs <- c(pch.1, pch.2, pch.3)
-		} else {
-			pchs <- rep(1, n)
-			if (sig) {
-				pchs[which(x$significant == 1)] <- 19
-			}
-		}
-	}
+	# TODO: set color/shading according to varnames
+	pb <- barplot(x$coef)
 	plot(1:n, x$coef, ylim = plot.range, xlab = "", ylab = "Coefficients", xaxt = "n", type = "n")
 	lines(c(0.5, (n + 0.5)), c(0, 0), lty = 2, col = "grey")
 	points(1:n, x$coef, pch = pchs)
