d4b43fd785a604480fa2916dd35d3a263ed5ebb2
Christian Zang <christian@dedalus.local>
2010-04-30 15:47:21 +0200

    mdcc.R shall hold wrapper function for shifting stats

---
+++ R/mdcc.R
@@ -0,0 +1,5 @@
+mdcc <- function(chrono, clim, method = "response", start = -6, end = 9, timespan = NULL, vnames = NULL, sb = TRUE, winsize = 25, offset = 1) {
+	# TODO: status bar should correspond to whole window movement, not to single computations
+	# TODO: all dcc with given parameters
+	# TODO: Loop through series.
+}
