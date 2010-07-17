2b8afe07de253678b6095dcefd5dcc6c49349998
Christian Zang <christian@dedalus.local>
2010-07-17 14:18:19 +0200

    very early functionality

--- R/dcbarplot.R
+++ R/dcbarplot.R
@@ -1,2 +1,7 @@
-dcbarplot <- function(x) {
+dcbarplot <- function(x, labels, ci = TRUE, sig = TRUE, col =
+                      "grey75", sig.col = "grey25") {
+  if (is.null(labels))
+    labels <- rownames(x)
+  n <- dim(x)[1]
+  bp <- barplot(x$coef)
 }
